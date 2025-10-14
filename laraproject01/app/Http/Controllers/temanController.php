b6af0fb6ddfe5eb3c6e3417561177a0ec966e34d
MhSTIKI <arthawebid@gmail.com>
2025-10-14 12:20:21 +0800

    update terakhir

--- laraproject01/app/Http/Controllers/temanController.php
+++ laraproject01/app/Http/Controllers/temanController.php
@@ -7,7 +7,24 @@ use Illuminate\Http\Request;
 class temanController extends Controller
 {
     public function index(){
-
-        return view('teman');
+        $dt = [
+            [
+                'idbuku'=>1,
+                'namateman'=>'Yuanes Idah Blili',
+                'alamat'=>'Jl. Lurus Terus Blok D.29A',
+                'kota'=>'Denpasar',
+                'telp'=>'03611011',
+                'wa'=>'08778877',
+            ],
+            [
+                'idbuku'=>2,
+                'namateman'=>'Yunita Sari dewi Purnama',
+                'alamat'=>'Jl. Lika-liku D No 29A',
+                'kota'=>'Denpasar',
+                'telp'=>'036445544',
+                'wa'=>'0866554433',
+            ],
+        ];
+        return view('teman',compact('dt') ) ;
     }
 }
